810c6d9e67433f4f2a673f0c4fab503c6d13a5f4
Anurag Sarkar <anurag.sarkar@oyorooms.com>
2019-01-29 11:08:45 +0530

    base directories ...

---
+++ cmake-build-default/CMakeFiles/Hailey.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Hailey.
+# This may be replaced when dependencies are built.
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    std::cout << "Hello, World! to my programming practice ..." << std::endl;
+    std::cout << "CAUTION this is not meant for competitive coding or to crack any interview ... " << std::endl;
+    std::cout << "this is to keep ME busy when my friends are out drinking ......" << std::endl;
+    return 0;
+}
